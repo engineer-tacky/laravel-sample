67fb06b78c404b1fdf61fb04a871bca41bdc5f57
engineer_tacky <tsubasa801@gmail.com>
2023-06-22 22:26:42 +0900

    シーダー、ファクトリーを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Author;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-      $this->call(CategoriesTableSeeder::class);
+      $this->call([
+        AuthorsTableSeeder::class,
+        BooksTableSeeder::class,
+        AuthorBookTableSeeder::class,
+      ]);
     }
 }
