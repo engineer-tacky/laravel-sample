5ec41e63ba5dbe98ddb41cd5510bb3d0d9ec29ec
engineer_tacky <tsubasa801@gmail.com>
2023-06-23 20:09:57 +0900

    書籍データ登録処理追加

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
 use App\Models\Book;
+use App\Models\Category;
 
 class BookController extends Controller
 {
@@ -17,5 +19,23 @@ class BookController extends Controller
     public function show(string $id): Book
     {
         return Book::findOrFail($id);
+    }
+
+    public function create(): View
+    {
+        $categories = Category::all();
+
+        return view('admin/book/create', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $book = new Book();
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->save();
+
+        return $book;
     }
 }
