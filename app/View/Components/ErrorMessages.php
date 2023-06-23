1975daa0451b832471a41bee3dd7f8457600913f
engineer_tacky <tsubasa801@gmail.com>
2023-06-23 20:55:54 +0900

    コンポーネント分割

---
+++ app/View/Components/ErrorMessages.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\Support\ViewErrorBag;
+
+class ErrorMessages extends Component
+{
+    public ViewErrorBag $errors;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(ViewErrorBag $errors)
+    {
+        $this->errors = $errors;
+    }
+
+    public function has2MoreErrors(): bool
+    {
+        return $this->errors->count() > 2;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.error-messages');
+    }
+}
