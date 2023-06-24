9bc22ed4d04a05ec4206f708b3795c85374454c8
engineer_tacky <tsubasa801@gmail.com>
2023-06-24 10:38:07 +0900

    削除処理を追加

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -82,4 +82,13 @@ class BookController extends Controller
           ->route('book.index')
           ->with('message', $book->title . 'を更新しました。');
     }
+
+    public function destroy(Book $book): RedirectResponse
+    {
+        $book->delete();
+
+        return redirect()
+          ->route('book.index')
+          ->with('message', $book->title . 'を削除しました。');
+    }
 }
