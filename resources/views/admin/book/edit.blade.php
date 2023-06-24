90688373e55997fb4392acbabd8e4c76eb544685
engineer_tacky <tsubasa801@gmail.com>
2023-06-24 10:31:45 +0900

    更新機能の修正

---
+++ resources/views/admin/book/edit.blade.php
@@ -0,0 +1,14 @@
+<x-layouts.book-manager title="書籍更新">
+  <h1>書籍更新</h1>
+  @if ($errors->any())
+    <x-alert class="danger">
+      <x-error-messages :$errors />
+    </x-alert>
+  @endif
+  <form action="{{ route('book.update', $book) }}" method="POST">
+    @csrf
+    @method('PUT')
+    <x-book-form :$categories :$authors :$book :$authorIds />
+    <input type="submit" value="送信">
+  </form>
+</x-layouts.book-manager>
