a3f86976fb844d5e5cb3e9375c6d08480b375ad3
engineer_tacky <tsubasa801@gmail.com>
2023-06-25 11:37:44 +0900

    エラーページ追加

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>404 Not Found</title>
+</head>
+<body>
+  <h1>404 Not Found</h1>
+  <p>指定されたページは見つかりません。</p>
+  <a href="{{ route('admin.create') }}">ログインへ</a>
+</body>
+</html>
